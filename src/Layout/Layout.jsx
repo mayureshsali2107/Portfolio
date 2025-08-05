81d039e67a963960ba73a49c9902033645ec6bea
mayureshsali210 <you@example.com>
2025-08-05 16:22:19 +0530

    Portfolio

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
+
 import Footer from '../components/footer';
 function Layout() {
   return (
